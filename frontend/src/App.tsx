9869913d715f1b820b5683c8f89b58b3c6932ebe
Alan <baranalaan@gmail.com>
2022-08-10 15:40:52 +0200

    Add Fetch Message Button and WelcomeMessage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [welcomeMessage, setWelcomeMessage] = useState();
+  const fetchMessage = () => {
+    axios.get("api/hello")
+        .then(response => response.data)
+        .then(setWelcomeMessage)
+  }
+
+  return <>
+    {welcomeMessage}
+    <button onClick={fetchMessage}>Fetch Message</button>
+  </>;
 }
 
 export default App;
