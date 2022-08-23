de9dabe3a787b2cfbf974269932bc0c61f6c37d4
Alan <baranalaan@gmail.com>
2022-08-23 15:36:54 +0200

    Add logout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,49 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   const [welcomeMessage, setWelcomeMessage] = useState();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [me, setMe] = useState<string>();
+
+  const fetchMe = () => {
+    axios.get("api/users/me")
+        .then(response => response.data)
+        .then(setMe)
+  }
+
+  useEffect(()=>{
+      fetchMe();
+  axios.interceptors.response.use(response => response, error => {
+    const status = error.response ? error.response.status : null
+    if (status === 401 && !error.config.auth) {
+      toast("Sitzung abgelaufen")
+      fetchMe()
+    }
+    return Promise.reject(error);
+  })
+},
+      []
+  )
+
+  const login = () => {
+    axios.get("api/users/login", {auth: {username, password: password}})
+        .then(response => response.data)
+        .then(setMe)
+        .catch(() => toast("Falsch"))
+  }
+
+  const logout = () => {
+    axios.get("api/users/logout")
+        .then(response => response.data)
+        .then(() => setMe("anonymousUser"))
+  }
+
   const fetchMessage = () => {
     axios.get("api/hello")
         .then(response => response.data)
@@ -13,8 +51,21 @@ function App() {
   }
 
   return <>
-    {welcomeMessage}
-    <button onClick={fetchMessage}>Fetch Message</button>
+    {
+      me ? (
+          me === 'anonymousUser' ?
+              <>
+                <input type={"text"} value={username} onChange={ev => setUsername(ev.target.value)}/>
+                <input type={"text"} value={password} onChange={ev => setPassword(ev.target.value)}/>
+                <button onClick={login}>Login!</button>
+              </> : <>
+                hi {me}
+                <button onClick={fetchMessage}>Get!</button>
+                <button onClick={logout}>Logout!</button>
+              </>
+      ) : "Loading..."
+    }
+    <ToastContainer/>
   </>;
 }
 
